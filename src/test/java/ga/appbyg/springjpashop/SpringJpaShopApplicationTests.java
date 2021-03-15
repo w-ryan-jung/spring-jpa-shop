7b7e131681d40f31532258445ff99e6e1bc40c64
w.ryan.jung <w.ryan.jung@gmail.com>
2021-03-15 23:16:54 +0100

    create repository

---
+++ src/test/java/ga/appbyg/springjpashop/SpringJpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.appbyg.springjpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
